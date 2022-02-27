61deebe708b343273380d7b21bb6b7e8ec0c358e
aeiou1102 <namm01020002@gmail.com>
2022-02-27 11:18:57 -0600

    Modificacion del index.js

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require("express");
-const db = require("./config/db");
+const routerApi = require('./routes');
 
-
+//const db = require("./config/db");
 const app = express();
-app.use(require("./routes/equipos-route"));
-app.use(require("./routes/huesos-route"));
-app.use(require("./routes/imagenes-route"));
-app.use(require("./routes/manada-route"));
-app.use(require("./routes/notas-route"));
-app.use(require("./routes/roles-route"));
-app.use(require("./routes/tareas-route"));
-app.use(require("./routes/usuario-route"));
+const port = 3000;
 
-const port = 3000 || process.env.PORT;
-db();
-app.listen(port, () =>{
-    console.log(`Servidor montado en puerto ${port}`);
-});
+// app.use(require("./routes/equipos-route"));
+// app.use(require("./routes/huesos-route"));
+// app.use(require("./routes/imagenes-route"));
+// app.use(require("./routes/manada-route"));
+// app.use(require("./routes/notas-route"));
+// app.use(require("./routes/roles-route"));
+// app.use(require("./routes/tareas-route"));
+// app.use(require("./routes/usuario-route"));
+
+app.use(express.json());
+routerApi(app);
+
+app.listen(port, () => {
+  console.log('Servidor montado en puerto ' + port);
+})
+
+// const port = 3000 || process.env.PORT;
+// db();
+// app.listen(port, () =>{
+//     console.log(`Servidor montado en puerto ${port}`);
+// });
