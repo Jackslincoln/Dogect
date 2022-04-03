9dd4ce84515e96ea179e6affc9063e3596734b53
Jackslincoln <96542952+Jackslincoln@users.noreply.github.com>
2022-04-02 22:56:18 -0600

    Corección de bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-<<<<<<< Updated upstream
-
-ReactDOM.render(<h1> HOLA SOY UN ENCABEZADO H1 </h1>, document.getElementById('app'))
-=======
 import { App } from './App';
 
 ReactDOM.render(
@@ -11,4 +7,3 @@ ReactDOM.render(
 <App/>,
 
 document.getElementById('app'))
->>>>>>> Stashed changes
