8e30d82244b7ddd973e3a3fad5ebe3c0c9e6cece
aeiou1102 <namm01020002@gmail.com>
2022-04-06 22:34:05 -0500

    Link hacia el login

--- dogect-frontend/src/landing/index.js
+++ dogect-frontend/src/landing/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Fondo, Centro, Title, Subtitle, Button2} from './styles';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,11 +12,13 @@ export const Landing = (
 ) =>
 (
     <div>
-        <Fondo img={"https://imgur.com/Bb1Flul.jpeg"}>
+        <Fondo img={"https://imgur.com/Bb1Flul.jpeg"} >
             <Centro>
                 <Title>Dogect</Title>
                 <Subtitle>Descubre la nueva forma de planificar tus proyectos</Subtitle>
-                <Button2>¡Únete gratis!</Button2>
+                <Link to={"/signup"} style={{textDecoration:"none"}}>
+                  <Button2>¡Únete gratis!</Button2>
+                </Link>
             </Centro>
         </Fondo>
 
--- dogect-frontend/src/navbar/index.js
+++ dogect-frontend/src/navbar/index.js
@@ -7,20 +7,20 @@ import {Ulx, Lix, Lix2, Img, Button1, Button2, Divbuttons} from './styles';
 
 export const Navbarl = (
 {
-    
+
 	//Estos son los default
 	texto1="ENTRA",
     texto2="REGÍSTRATE",
     path="http://www.google.com",
     path2="http://wwwyoutube.com"
 
-    
+
 }
 ) =>
 (
-    
- 
-        <div>        
+
+
+        <div>
             <Ulx>
                 <Lix><Img src="https://i.imgur.com/CZPzCt0.png"></Img></Lix>
                 <Lix2>
@@ -28,13 +28,13 @@ export const Navbarl = (
                     <Button1  href={path}>{texto1}</Button1>
                     <Button2  href={path2}>{texto2}</Button2>
                     </Divbuttons>
-                    
-                </Lix2>    
+
+                </Lix2>
             </Ulx>
         </div>
-   
-    
-	
-    
+
+
+
+
     //<Col><Button variant={color} href={path}><h1>{text}</h1> <FontAwesomeIcon icon = {icons[iconName]}></FontAwesomeIcon></Button></Col>
 );
