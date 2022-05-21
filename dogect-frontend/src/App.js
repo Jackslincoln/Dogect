ef18317eab7862aa5ecd344bb83b879dde7369ab
aeiou1102 <namm01020002@gmail.com>
2022-05-21 16:39:56 -0500

    limpieza de una parte

--- dogect-frontend/src/App.js
+++ dogect-frontend/src/App.js
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
-import { Sidebar } from "./sidebar";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import {HomePage} from './components/pages/HomePage';
-import LoginPage from './components/pages/LoginPage';
-import SignupPage from "./components/pages/SignupPage";
-import NotFoundPage from './components/pages/NotFoundPage';
-import LandingPage from './components/pages/LandingPage';
-import Equipo from "./equipo/Equipo";
 
-import Footer from './components/static/Footer/Footer';
 import { Helmet } from "react-helmet";
-import Navbarl  from './components/static/navbar/index';
 import AppRouter from './routers/AppRouter';
 import AuthProvider from "./auth/AuthProvider";
 
@@ -30,42 +20,6 @@ function App() {
       <AuthProvider>
         <AppRouter></AppRouter>
       </AuthProvider>
-
-
-
-
-
-
-
-
-
-     {/* <Router>
-       <Navbarl/>
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/landing" element={<LandingPage/>}></Route>
-          <Route exact path="/login" element={<LoginPage/>}></Route>
-          <Route exact path="/signup" element={<SignupPage/>}></Route>
-          <Route exact path="/equipo" element={<Equipo/>}></Route>
-
-          <Route path="*" element={<NotFoundPage />}></Route>
-        </Routes>
-        <Footer/>
-     </Router> */}
-
-
-
-    {/*--- EL ORDEN DE TODO ---*/}
-    {/* Home */}
-      {/* Navbar */}
-        {/* Banner */}
-        {/* User Info */}
-        {/* Cards */}
-      {/* Footer */}
-
-    {/* Manada */}
-      {/* Navbar */}
-      {/* Footer */}
     </div>
   );
 }
